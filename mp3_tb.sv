8c62d54d688df1ac121d92625806c3480bff1d52
Stephen Salerno <ssalern2@gelib-057-25.ews.illinois.edu>
2015-03-15 21:08:20 -0500

    project files

---
+++ mp3_tb.sv
@@ -0,0 +1,43 @@
+module mp3_tb;
+
+timeunit 1ns;
+timeprecision 1ns;
+
+logic clk;
+logic mem_resp;
+logic mem_read;
+logic mem_write;
+logic [1:0] mem_byte_enable;
+logic [15:0] mem_address;
+logic [15:0] mem_rdata;
+logic [15:0] mem_wdata;
+
+/* Clock generator */
+initial clk = 0;
+always #5 clk = ~clk;
+
+mp3 dut
+(
+    .clk,
+    .mem_resp,
+    .mem_rdata,
+    .mem_read,
+    .mem_write,
+    .mem_byte_enable,
+    .mem_address,
+    .mem_wdata
+);
+
+memory memory
+(
+    .clk,
+    .read(mem_read),
+    .write(mem_write),
+    .byte_enable(mem_byte_enable),
+    .address(mem_address),
+    .wdata(mem_wdata),
+    .resp(mem_resp),
+    .rdata(mem_rdata)
+);
+
+endmodule : mp3_tb
