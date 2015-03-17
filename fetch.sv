806c73322a9452d3387e69e93a64b6c6c23e7af7
stps <stephenlsalerno@gmail.com>
2015-03-17 14:27:15 -0500

    copied things over

---
+++ fetch.sv
@@ -0,0 +1,14 @@
+import lc3b_types::*;
+
+module fetch
+(
+	input clk,
+	input pc_mux_sel,
+
+	output new_pc,
+	output ir,
+	output valid,
+	output stall
+);
+
+endmodule : fetch
---
+++ mux2.sv
@@ -0,0 +1,14 @@
+module mux2 #(parameter width = 16)
+(
+			input sel,
+         input [width-1:0] a, b,
+         output logic [width-1:0] out
+);
+         always_comb
+         begin
+             if (sel == 0)
+                 out = a;
+				else
+					out = b;
+         end
+         endmodule : mux2
