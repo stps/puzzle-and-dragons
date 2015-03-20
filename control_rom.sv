425368101df0fc8cb65f20dee193a59aa86f0172
daosyn <elmobey@gmail.com>
2015-03-19 19:59:49 -0500

    basement soon

--- control_rom.sv
+++ control_rom.sv
@@ -12,56 +12,56 @@ begin
 
 /* Default assignments */
 ctrl.opcode = opcode;
-ctrl.load_cc = 0;
-ctrl.load_regfile = 0;
-ctrl.sr2mux_sel = 0;
-ctrl.mem_read = 0;
-ctrl.mem_write = 0;
-ctrl.addr1mux_sel = 0;
+ctrl.load_cc = 1'b0;
+ctrl.load_regfile = 1'b0;
+ctrl.sr2mux_sel = 1'b0;
+ctrl.mem_read = 1'b0;
+ctrl.mem_write = 1'b0;
+ctrl.addr1mux_sel = 1'b0;
 ctrl.addr2mux_sel = 2'b00;
 
 /* Assign control signals based on opcode */
 case(opcode)
     op_add: begin
         ctrl.aluop = alu_add;
-        ctrl.load_regfile = 1;
+        ctrl.load_regfile = 1'b1;
         if(imm_check == 0) begin
-            ctrl.sr2mux_sel = 0;
+            ctrl.sr2mux_sel = 1'b0;
         end
         else begin
-            ctrl.sr2mux_sel = 1;
+            ctrl.sr2mux_sel = 1'b1;
         end
-        ctrl.load_cc = 1;
+        ctrl.load_cc = 1'b1;
     end
     
     op_and: begin
         ctrl.aluop = alu_and;
-        ctrl.load_regfile = 1;
+        ctrl.load_regfile = 1'b1;
         if(imm_check == 0) begin
-            ctrl.sr2mux_sel = 0;
+            ctrl.sr2mux_sel = 1'b0;
         end
         else begin
-            ctrl.sr2mux_sel = 1;
+            ctrl.sr2mux_sel = 1'b1;
         end
-        ctrl.load_cc = 1;
+        ctrl.load_cc = 1'b1;
     end
     
     op_not: begin
         ctrl.aluop = alu_not;
-        ctrl.load_regfile = 1;
-        ctrl.load_cc = 1;
+        ctrl.load_regfile = 1'b1;
+        ctrl.load_cc = 1'b1;
     end
     
     op_ldr: begin
-        ctrl.mem_read = 1;
-        ctrl.load_cc = 1;
-        ctrl.addr1mux_sel = 1;
+        ctrl.mem_read = 1'b1;
+        ctrl.load_cc = 1'b1;
+        ctrl.addr1mux_sel = 1'b1;
         ctrl.addr2mux_sel = 2'b01;
     end
     
     op_str: begin
-        ctrl.mem_write = 1;
-        ctrl.addr1mux_sel = 1;
+        ctrl.mem_write = 1'b1;
+        ctrl.addr1mux_sel = 1'b1;
         ctrl.addr2mux_sel = 2'b01;
     end
     
