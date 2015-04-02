022b49efe8f87ce413bba3e089d5e95d83fadbf6
stps <stephenlsalerno@gmail.com>
2015-04-02 10:45:39 -0500

    so ez

--- fetch_stall_logic.sv
+++ fetch_stall_logic.sv
@@ -15,13 +15,46 @@ module fetch_stall_logic
 );
          always_comb
          begin
-				if (dep_stall == 1'b1 || decode_br_stall == 1'b1 || execute_br_stall == 1'b1 || mem_stall == 1'b1 || mem_br_stall == 1'b1 || icache_resp == 1'b0)
-				begin
-					load_de = 1'b0;
-				end
-				else
+				if (decode_br_stall == 1'b1) // DE stage tells fetch to insert bubbles until the PC is updated in MEM
 				begin
 					load_de = 1'b1;
+					valid = 1'b0;
+				end
+				
+				else if (dep_stall == 1'b1) // DE stage tells fetch to stall, and EX to insert bubbles
+				begin
+					load_de = 1'b0;
+					valid = 1'b0;
+				end
+				
+				else if (execute_br_stall == 1'b1) // control instruction still propogating, still insert bubbles
+				begin
+					load_de = 1'b1;
+					valid = 1'b0;
+				end
+				
+				else if (mem_stall == 1'b1) // stall everything except WB, insert bubbles in WB
+				begin
+					load_de = 1'b0;
+					valid = 1'b0;
+				end
+				
+				else if (mem_br_stall == 1'b1) // control instruction still propogating, still insert bubble
+				begin
+					load_de = 1'b1;
+					valid = 1'b0;
+				end
+				
+				else if (icache_resp == 1'b0) // while waiting for cache miss, just stall fetch
+				begin
+					load_de = 1'b0;
+					valid = 1'b0;
+				end
+				
+				else // otherwise everything is fine
+				begin
+					load_de = 1'b1;
+					valid = 1'b1;
 				end
          end
 endmodule : fetch_stall_logic
