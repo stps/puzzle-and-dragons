7fd5383092c424e86f8f58761159dd2d8268066e
daosyn <elmobey@gmail.com>
2015-04-15 22:39:12 -0500

    wip

--- forwarding_unit.sv
+++ forwarding_unit.sv
@@ -2,7 +2,20 @@ import lc3b_types::*;
 
 module forwarding_unit
 (
-    input lc3bword ex_mem_regdata,
-    input lc3bword mem_wb_regdata,
+    input lc3b_reg ex_mem_dr_out,
+    input lc3b_reg mem_wb_dr_out,
+    input lc3b_reg de_ex_rs_out,
+    input lc3b_reg de_ex_rt_out,
     
+    output [1:0] forwardA_mux_sel,
+    output [1:0] forwardB_mux_sel
 );
+
+always_comb
+begin
+    forwardA_mux_sel = 2b'00;
+    forwardB_mux_sel = 2b'00;
+    
+end
+
+endmodule : forwarding_unit
